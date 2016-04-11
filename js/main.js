bac5d0e74baa7d8e390f22f6ce145389ad02a4a6
jk <jkpas30@gmail.com>
2016-04-11 12:26:06 +0300

    done

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	    interval: false
 	}); 	
 
-// alax for php mailer --------------------   
+// ajax for php mailer --------------------   
 
 	$("#form").submit(function() { //устанавливаем событие отправки для формы с id=form
         var form_data = $(this).serialize(); //собераем все данные из формы
@@ -25,14 +25,7 @@ $(document).ready(function(){
 // adaptive ------------
 
 	if (window.matchMedia('(max-width: 992px)').matches){
-	    $(".advanteges_text").remove().insertAfter($(".advanteges h2"));
-	    $("#adaptive_row1_a1").remove().append($("#adaptive_row1"));
-	    $("#adaptive_row1_a2").remove().append($("#adaptive_row1_a1"));
-	    $("#adaptive_wrap1").remove();
-	    $("#adaptive_row1").after("<div class="row" id="adaptive_row2"></div>");
-	    $("#adaptive_row2_a1").remove().append($("#adaptive_row2"));
-	    $("#adaptive_row2_a2").remove().append($("#adaptive_row1_a1"));
-	    $("#adaptive_wrap2").remove();	
+	    $(".advanteges .row").removeClass("marg_bot_90");	    
 	};
 
 
